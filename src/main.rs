070e54a984fd86cdf7c78764c334900671f4817c
rohitmusti <rohit.musti.rm@gmail.com>
2024-01-07 11:46:52 -0800

    got the file not found error implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use clap::Parser;
+use std::error;
 
-//#[derive(Debug, Deserialize)]
-//struct Config {
-//    api_token: String,
-//}
+use std::fmt;
+
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fs;
+use toml;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    jira_api_token: String,
+}
 
 #[derive(Debug, Parser)]
 #[clap(
@@ -18,7 +26,36 @@ struct ActivityTree {
     describe_ticket: bool,
 }
 
+const CONFIG_PATH: &str = "~/.config/jira-cli/config.toml";
+
+fn read_app_config() -> Result<Config, Box<dyn Error>> {
+    if !fs::metadata(CONFIG_PATH).is_ok() {
+        let error_message = format!(
+            "Error 💾 🚫 🔍: could not find a config file at {}, please create one!",
+            CONFIG_PATH
+        );
+        return Err(std::io::Error::new(std::io::ErrorKind::NotFound, error_message).into());
+    }
+
+    let toml_content = fs::read_to_string(CONFIG_PATH)?;
+    let deserialized_config: Config = toml::from_str(&toml_content)?;
+    Ok(deserialized_config)
+}
+
 fn main() {
+    //let app_config: Config;
+    match read_app_config() {
+        Ok(config) => {
+            //app_config = config;
+            println!("{:?}", config);
+        }
+        Err(err) => {
+            eprintln!("{}", err);
+            return;
+        }
+    }
+    //print!("{:?}", app_config);
+
     let args = ActivityTree::parse();
 
     match args.jira_api_token {
@@ -28,7 +65,6 @@ fn main() {
         None => {
             print!("no jira api token provided :(\n");
         }
-
     }
 
     if args.describe_ticket {
@@ -36,6 +72,4 @@ fn main() {
     } else {
         print!("do not need to describe a ticket\n");
     }
-    
 }
-
