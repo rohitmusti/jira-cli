6c3864638e2bfa4cb18925cd4f580f8732f472e6
rohitmusti <rohit.musti.rm@gmail.com>
2024-01-07 13:26:14 -0800

    better debugs and avoiding cloning

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use clap::Parser;
-use std::error;
-
-use std::fmt;
-
 use serde::{Deserialize, Serialize};
+use std::error;
+use std::env::var;
+use std::fmt;
 use std::error::Error;
 use std::fs;
 use toml;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct Config {
     jira_api_token: String,
 }
@@ -26,9 +25,14 @@ struct ActivityTree {
     describe_ticket: bool,
 }
 
-const CONFIG_PATH: &str = "~/.config/jira-cli/config.toml";
 
 fn read_app_config() -> Result<Config, Box<dyn Error>> {
+
+    // TODO: figure out how to pull this from a ~.config/jira-cli/config.toml file
+    //const CONFIG_PATH: String = format!("{}/.config/jira-cli/config.toml", var("HOME"));
+    const CONFIG_PATH: &str = "/users/rohitmusti/.config/jira-cli/config.toml";
+    
+    // first check if the file actually exists
     if !fs::metadata(CONFIG_PATH).is_ok() {
         let error_message = format!(
             "Error 💾 🚫 🔍: could not find a config file at {}, please create one!",
@@ -37,39 +41,41 @@ fn read_app_config() -> Result<Config, Box<dyn Error>> {
         return Err(std::io::Error::new(std::io::ErrorKind::NotFound, error_message).into());
     }
 
+    // read in the file as a string
     let toml_content = fs::read_to_string(CONFIG_PATH)?;
+
+    // deserialze data into the config object
     let deserialized_config: Config = toml::from_str(&toml_content)?;
+
     Ok(deserialized_config)
 }
 
 fn main() {
-    //let app_config: Config;
-    match read_app_config() {
-        Ok(config) => {
-            //app_config = config;
-            println!("{:?}", config);
-        }
+
+    // try and grab the config for the CLI app, exit if an error is raised
+    let app_config = match read_app_config() {
+        Ok(config) => config.clone(),
         Err(err) => {
-            eprintln!("{}", err);
+            println!("{}", err);
             return;
         }
-    }
-    //print!("{:?}", app_config);
+    };
 
     let args = ActivityTree::parse();
 
     match args.jira_api_token {
         Some(jira_api_token) => {
-            print!("jira api token: {}\n", jira_api_token);
+            println!("jira api token provided: {}", jira_api_token);
+            println!("going to replace the value in the config w/ the provided value");
         }
         None => {
-            print!("no jira api token provided :(\n");
+            println!("no jira api token provided using the config: {}", app_config.jira_api_token);
         }
     }
 
     if args.describe_ticket {
-        print!("need to describe a ticket\n");
+        println!("need to describe a ticket");
     } else {
-        print!("do not need to describe a ticket\n");
+        println!("do not need to describe a ticket");
     }
 }
